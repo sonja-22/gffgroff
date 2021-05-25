352e9c8192829617d9ece6981b62307d454d3008
Todd Sonjiku <empty@sonjiku.xyz>
2021-05-25 22:41:27 +0300

    Reconverted fonts. All should work now
    Signed-off-by: Todd Sonjiku <empty@sonjiku.xyz>

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 
+if [ -f /usr/share/groff/current/font/devps/FSER ]; then 
+	find /usr/share/groff/current/font/devps/ -name "FSE*" -exec rm -rf {} \;
+fi
 if command -v gsx >/dev/null 2>&1 /dev/null; then
 	find Font/ -name "F*" -exec cp {} /usr/share/ghostscript/*/Resource/Font/ \;
 	find devps/ -name "F*" -exec cp {} /usr/share/groff/current/font/devps/ \;
